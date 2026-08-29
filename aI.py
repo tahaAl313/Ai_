bff4deab260349190d2fe29487fadb0005e1a4d0
tahaAl313 <ttttaha977@gmail.com>
2026-08-29 17:42:05 +0330

    Create aI.py

---
+++ aI.py
@@ -0,0 +1,13 @@
+
+my_memory = {
+    "text": "می‌خوام امنیتِ سیستم رو بسازم",
+    "topic": "امنیت",
+    "importance": 10
+}
+
+print("--- اولین خاطره‌ی هوش مصنوعی ---")
+print(f"متن خاطره: {my_memory['text']}")
+print(f"موضوع: {my_memory['topic']}")
+print(f"میزان اهمیت: {my_memory['importance']}/10")
+print("-------------------------------")
+
