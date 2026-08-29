7eee7fad323ca78963d729f5b8c0bdc019f913b7
tahaAl313 <ttttaha977@gmail.com>
2026-08-29 08:42:06 +0330

    Create AI_core.py

---
+++ AI_core.py
@@ -0,0 +1,14 @@
+def activation():
+    print("سیستمِ هوشمندِ طاها فعال شد.")
+    print("در حالِ اسکنِ پروتکل‌هایِ امنیتی...")
+
+    password = input(" لطفاً رمز عبور امنیتی را وارد کنید:  ")
+
+    if password == "taha313" :
+        print("خوش اومدی.")
+        print("چه خبر.")
+    else:
+        print("خطا رمز اشتباه.")
+        print("سیستم قفل شد.")
+
+activation()
